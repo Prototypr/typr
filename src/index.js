378867c6e7afabc23e8f97e140c637ed7d62bfc1
Graeme Fulton <graeme@prototypr.io>
2024-08-02 20:48:31 +0100

    new version

--- src/index.js
+++ src/index.js
@@ -6,7 +6,43 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./page-styles.scss";
  
 import EditorWrapper from './EditorWrapper';
-// other imports...
+import MenuFloating from './Menus/FloatingMenu';
+import ImageMenu from './Menus/ImageMenu';
+import Cite from './CustomExtensions/Cite'
+import Iframe from './CustomExtensions/Iframe/Iframe'
+import { Blockquote } from './CustomExtensions/CustomBlockquote';
+import {Image} from './CustomExtensions/Figure2/CustomImage'
+import Figure from './CustomExtensions/Figure2/Figure'
+import FigCaption from './CustomExtensions/Figure2/Figcaption';
+import {PasteFilter} from './CustomExtensions/PasteFilter'
+import Tweet from './CustomExtensions/Tweet/Tweet'
+import LinkEmbed from './CustomExtensions/LinkEmbed/LinkEmbed';
+import Video from './CustomExtensions/Video/Video';
+import VideoMenu from './Menus/VideoMenu';
+import { addTwitterScript } from './editorHooks/libs/addTwitterScript';
+import UndoRedoButtons from './UndoRedoButtons';
+import EditorNavButtons from './EditorNavButtons';
+import LinkInput from './Menus/MenuButtons/LinkDropdown/LinkButtonRadix';
 
 
 export default EditorWrapper;
+
+export {
+  MenuFloating,
+  ImageMenu,
+  VideoMenu,
+  Cite,
+  Iframe,
+  Blockquote,
+  Image,
+  Figure,
+  FigCaption,
+  PasteFilter,
+  Tweet,
+  LinkEmbed,
+  Video,
+  addTwitterScript,
+  UndoRedoButtons,
+  EditorNavButtons,
+  LinkInput
+};
